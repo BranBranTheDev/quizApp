abcf8b4653fe56b758026f4481712280a877d73a
Brandon <bran9bran6@gmail.com>
2023-11-21 21:17:18 -0700

    deleted express and axios, I did not need these dependencies and added OpenAI noddeJS libraries

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
-const express = require('express');
-const axios = require('axios');
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
 require('dotenv').config();
 
-const app = express();
-const port = 3000;
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-app.get('/trivia', async (req, res) => {
-    const prompt = 'Generate a trivia question about JavaScript';
-    const maxTokens = 60;
+ 
 
-    try {
-        const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-            prompt,
-            max_tokens: maxTokens,
-        }, {
-            headers: {
-                'Authorization': `Bearer ${OPENAI_API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
 
-        const triviaQuestion = response.data.choices[0].text.trim();
-        res.send(triviaQuestion);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred while retrieving trivia question.');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-});
 
